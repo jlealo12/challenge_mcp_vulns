7ec67386d3c1f11150325bbb8cf0c002790d3ed9
Jorjhan Leal Ortiz <jorjhan@LAPTOP-C4J19HQ5>
2025-10-10 23:01:00 -0500

    mcp: add debug message

--- mcp/main.py
+++ mcp/main.py
@@ -6,5 +6,11 @@ mcp = FastMCP("Meli MCP")
 def greet(name: str) -> str:
     return f"Hello, {name}!"
 
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    print("Executing call to add tool from MCP")
+    return a + b
+
 if __name__ == "__main__":
     mcp.run(transport="http", port=8000)
