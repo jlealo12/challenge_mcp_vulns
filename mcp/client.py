01ee84a57916d1c2641553ea95c7c520165d582a
jlealo12 <92340459+jlealo12@users.noreply.github.com>
2025-10-16 22:10:39 -0500

    mcp: update test client with new tools

--- mcp/client.py
+++ mcp/client.py
@@ -3,12 +3,19 @@ from fastmcp import Client
 
 client = Client("http://localhost:8000/mcp")
 
-async def call_tool(name: str):
+
+async def call_tool(fn_name: str, fn_input: dict = None):
     async with client:
-        # result = await client.call_tool("greet", {"name": name})
-        # result = await client.call_tool("list_emails")
-        # result = await client.call_tool("get_email", {"id": "email001"})
-        result = await client.call_tool("get_email", {"id": "email005"})
+        result = await client.call_tool(fn_name, fn_input)
         print(result)
 
-asyncio.run(call_tool("Ford"))
+
+asyncio.run(call_tool("list_emails"))
+asyncio.run(call_tool("get_email", {"id": "email001"}))
+asyncio.run(call_tool("get_email", {"id": "email005"}))
+asyncio.run(
+    call_tool(
+        "send_email",
+        {"to": "ejemplo@ejemplo.com", "subject": "Ejemplo", "body": "Body ejemplo"},
+    )
+)
